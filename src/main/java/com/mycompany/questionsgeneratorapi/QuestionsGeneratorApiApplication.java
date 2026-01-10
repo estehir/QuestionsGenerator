8da78f7e5bcd3ea438c8032a693075e58884bc2f
Estefanio Hirsch <estefanioh@gmail.com>
2026-01-10 15:16:10 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/questionsgeneratorapi/QuestionsGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.questionsgeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsGeneratorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionsGeneratorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/questionsgeneratorapi/QuestionsGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.questionsgeneratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsGeneratorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
